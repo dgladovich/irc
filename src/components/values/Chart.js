17f7ebce6fb1927fdc4a1e1152dbc0801cf580b3
dgladovich <dgladovich@gmail.com>
2018-05-15 15:06:09 +0300

    chart

--- src/components/values/Chart.js
+++ src/components/values/Chart.js
@@ -1,7 +1,6 @@
 import template from './templates/chart.jst';
 import {View, Model} from 'backbone.marionette';
 import Chart from 'chart.js';
-import 'chartjs-plugin-streaming';
 import moment from 'moment';
 import faker from 'faker';
 
@@ -10,9 +9,19 @@ export default View.extend({
     className: 'serial-chart',
     updateChart: function () {
         let {labels, datasets} = this.chart.data;
-        let maxLabels = 1000;
+        const MAX_LABELS = 30;
+        const MAX_TICKS = 450;
         let time = moment().format('HH:MM:ss');
-        if (labels.length < maxLabels) {
+        datasets.forEach((set) => {
+            let data = set.data;
+            if(data.length < MAX_TICKS){
+                data.unshift(this.model.get('def'));
+            } else {
+                data.unshift(this.model.get('def'));
+                data.pop();
+            }
+        })
+/*        if (labels.length < MAX_LABELS) {
             labels.push(time);
             datasets.forEach((set) => {
                 let data = set.data;
@@ -26,29 +35,42 @@ export default View.extend({
                 data.shift();
                 data.push(this.model.get('def'))
             })
-        }
+        }*/
         this.chart.update();
     },
     onDestroy: function () {
         clearInterval(this.updatingChart);
     },
+    onPanLeft: function(){
+        console.log('panned left')
+    },
+    onPanRight: function(){
+        console.log('panned')
+    },
+    _bindTouchEvents: function(){
+        let mc = new Hammer(this.el);
+        mc.on("panleft", this.onPanLeft.bind(this));
+        mc.on("panright", this.onPanRight.bind(this));
+    },
     onRender: function () {
         let ctx = this.$('.canvas-chart')[0].getContext('2d');
         let labelText = this.model.get('translate');
-
-        console.log(faker)
+        let labels = [];
+        labels.length = 500;
+        labels.fill('');
         this.chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: [],
+                labels: labels,
                 datasets: [{
                     label: labelText,
                     data: [],
                     backgroundColor: [
-                        faker.commerce.color()
+                        'rgba(225,44,13,0)'
                     ],
                     borderColor: [
-                        'rgba(225,44,13,1)'
+                        faker.commerce.color(),
+                        //'rgba(225,44,13,1)'
                     ],
                     borderWidth: 2
                 }]
@@ -56,7 +78,6 @@ export default View.extend({
             options: {
                 scales: {
                     xAxes: [{
-                        type: 'time',
                         ticks: {
                             autoSkip: true,
                             maxTicksLimit: 20
@@ -64,22 +85,23 @@ export default View.extend({
                     }],
                     yAxes: [{
                         ticks: {
-                            beginAtZero: true,
+                            //beginAtZero: true,
                         }
                     }]
                 },
                 responsive: true,
                 maintainAspectRatio: false,
-                animation: {
-                    duration: 1000 * 1,
-                    easing: 'linear'
-                },
+                //animation: false,
                 legend: false,
+                elements: { point: { radius: 0 } }
             }
         });
         this.updatingChart = setInterval(this.updateChart.bind(this), 1000);
+        this._bindTouchEvents.call(this);
     },
     initialize: function () {
+        this.store = [];
+        this.MAX_SHOW = 1;
         this.listenTo(this.model, 'change:def', this.updateChart.bind(this))
     }
 });
