c4e52a3b447b300a906d28b03d930a8ee1c11cf0
dgladovich <dgladovich@gmail.com>
2018-05-24 16:38:14 +0300

    configing chart

--- routes/values.js
+++ routes/values.js
@@ -1,24 +1,51 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const Datastore = require('nedb');
+const db = require('../models');
+const {Registering, Sequelize} = db;
+const Op = Sequelize.Op;
+const faker = require('faker');
 const moment = require('moment');
+const config = require('../config.json');
 const _ = require('lodash');
+const faces = _.toArray(config.ctrl.cfaces);
 
-
-router.get('/', function(req, res, next) {
-    const db = new Datastore({filename: 'register', autoload: true});
+router.get('/', function (req, res, next) {
     const {from, to} = req.query;
-    console.log(from, to, req.query);
-    if(req.query !== undefined){
-        db.find({}, (err, docs)=>{
-            if(err){console.error('Some error while reading stuff')}
-            const shit = _.filter(docs, (doc)=>{return moment(doc.created_at).isBetween(from, to)});
-            res.send(shit)
+    const PAGE_NUMBER = req.query.page || 1;
+    const ITEMS_PER_PAGE = 100;
+    const OFFSET = ITEMS_PER_PAGE * (PAGE_NUMBER - 1);
+    if (from && to) {
+        Registering
+            .findAll({
+                where: {
+                    created_at: {[Op.between]: [from, to]}
+                },
+                limit: ITEMS_PER_PAGE,
+                order: [['created_at', 'DESC']],
+                offset: OFFSET,
+            })
+            .then((values) => {
+                res.json(values)
+            })
+    } else {
+        res.send('not used parameters');
+    }
+}).get('/seed', function (req, res, next) {
+    let values = [];
+    for (let i = 0; i < 1000000; i++) {
+        const face = _.sample(faces);
+        values.push({
+            face_id: face.id,
+            def: _.random(face.min_val, face.max_val),
+            created_at: faker.date.between('2018-01-01', '2018-12-31')
         })
     }
-
-
+    Registering
+        .bulkCreate(values)
+        .then(() => {
+            res.send('Seeded successfully')
+        })
 });
 
 
